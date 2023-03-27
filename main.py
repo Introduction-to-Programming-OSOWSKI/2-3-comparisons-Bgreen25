ecf9485018514079a976fd3a5daddb8722aa7ac7
Bgreen25 <820129@mystma.org>
2023-03-27 14:44:11 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
-#WRITE YOUR CODE IN THIS FILE
+def greaterThan(x,y):
+    if x>y:
+        return "true"
+    else:
+        return "false"
+
+print (greaterThan(8,9))
+
+def lessThan(x,y):
+    if x<y:
+        return "true"
+    else:
+        return "false"
+    
+print (lessThan(7,9))
+
+def equalTo(x,y):
+    if x==y:
+        return "true"
+    else:
+        return "false"
+
+print (equalTo(9,9))
+
+def greaterOrEqual(x,y):
+    if x>=y:
+        return "true"
+    else:
+        return "false"
+
+print (greaterOrEqual(9,2))
+
+def lessOrEqual(x,y):
+    if x<=y:
+        return "true"
+    else:
+        return "false"
+
+print (lessOrEqual(9,8))
