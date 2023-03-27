56f3d9b60e8994bc57b258affaf996f4c2fa246f
Bgreen25 <820129@mystma.org>
2023-03-27 14:45:45 -0500

    2

--- main.py
+++ main.py
@@ -1,39 +1,39 @@
 def greaterThan(x,y):
     if x>y:
-        return "true"
+        return "True"
     else:
-        return "false"
+        return "False"
 
 print (greaterThan(8,9))
 
 def lessThan(x,y):
     if x<y:
-        return "true"
+        return "True"
     else:
-        return "false"
+        return "False"
     
 print (lessThan(7,9))
 
 def equalTo(x,y):
     if x==y:
-        return "true"
+        return "True"
     else:
-        return "false"
+        return "False"
 
 print (equalTo(9,9))
 
 def greaterOrEqual(x,y):
     if x>=y:
-        return "true"
+        return "True"
     else:
-        return "false"
+        return "False"
 
 print (greaterOrEqual(9,2))
 
 def lessOrEqual(x,y):
     if x<=y:
-        return "true"
+        return "True"
     else:
-        return "false"
+        return "False"
 
 print (lessOrEqual(9,8))
